e8ef3d1a9fb9ab59026b87493f4416a3f5271982
Walter Reyes <wal.reyes.11@gmail.com>
2017-10-27 16:48:36 -0500

    Adds clone cli command
    Implements a slightly different version of build-blueprint-commands and build-blueprint-command.
    
    If the cli will have more commands it might be a good idea to make those two files adaptable to different descriptions, usages, etc.

---
+++ src/cli/cmds/clone.js
@@ -0,0 +1,35 @@
+import buildBlueprintCommands from './clone/build-blueprint-commands';
+
+const usage = `Usage:
+  $0 clone <blueprint> <name>
+  $0 help clone <blueprint>`;
+
+module.exports = {
+  command: 'clone <blueprint> <name>',
+  aliases: ['g', 'gen'],
+  describe: 'Clone a blueprint with a different name',
+  builder: yargs => {
+    yargs
+      .usage(usage)
+      .option('dry-run', {
+        alias: 'd',
+        describe: "List files but don't generate them",
+        type: 'boolean'
+      })
+      .option('verbose', {
+        alias: 'v',
+        describe: 'Verbose output, including file contents',
+        type: 'boolean'
+      })
+      .group(['dry-run', 'verbose', 'help'], 'Generate Options:')
+      .updateStrings({
+        'Commands:': 'Blueprints:',
+        'Options:': 'Blueprint Options:'
+      });
+    return buildBlueprintCommands().reduce(
+      (yargs, command) => yargs.command(command),
+      yargs
+    );
+  },
+  handler: argv => console.error(`Unrecognised blueprint '${argv.blueprint}'`)
+};
